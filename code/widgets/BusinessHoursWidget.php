744528cfcbb0f9af895f7504d71d4f6f1cac6a23
Mark <mgb16nz@gmail.com>
2016-12-06 22:15:08 +1300

    Changes to BusinessHoursWidget.php

--- code/widgets/BusinessHoursWidget.php
+++ code/widgets/BusinessHoursWidget.php
@@ -7,50 +7,70 @@
 
 class BusinessHoursWidget extends Widget
 {
-
+    /**
+     * @var string
+     */
     private static $title = 'Business Hours';
 
+    /**
+     * @var string
+     */
     private static $cmsTitle = 'Business Hours';
 
+    /**
+     * @var string
+     */
     private static $description = 'Displays business hours of the week.';
 
-
+    /**
+     * @var array
+     */
     private static $db = array(
         'DaysToShow' => 'Int'
     );
 
-
+    /**
+     * @var array
+     */
     private static $defaults = array(
-        'DaysToShow' => 7
+        'NumberOfDays' => 7
     );
 
+    /**
+     * @var array
+     */
     private static $has_one = array(
-        'BusinessHours' => 'BusinessHours'
+        'BusinessHours' => 'BusinessHours',
     );
 
     public function getCMSFields()
     {
+        $fields = parent::getCMSFields();
+
         $this->beforeUpdateCMSFields(function ($fields) {
 
+            /**
+             * @var FieldList $fields
+             */
             $fields->merge(array(
-                DropdownField::create('BusinessHoursID', _t('BusinessHoursWidget.BusinessHours', 'Business Hours'), BusinessHours::get()->map()),
-                NumericField::create('DaysToShow', _t('BusinessHoursWidget.DaysToShow', 'Show working days of the week are open (left blank will show all days of the week)'))
+
+                NumericField::create(
+                    'DaysToShow', _t('BusinessHoursWidget.DaysToShow.Label', 'Working days of the week'), 0
+                )
+                    ->setDescription(_t('BusinessHoursWidget.DaysToShow.Description', 'Show number of Days Working (set to 0 to show all Days of the week).'))
             ));
         });
 
-        return parent::getCMSFields();
+        return $fields;
     }
 
-    public function getBusinessHours()
+    /**
+     * @return DataList
+     */
+    public function Hours()
     {
-        $hours = $this->BusinessHours();
+        return $this->BusinessHours();
 
-        if ($hours) {
-            return $hours->BusinessHours()
-                ->limit($this->DaysToShow);
-        }
-
-        return array();
     }
 }
 
